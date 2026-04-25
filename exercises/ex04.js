26e88b2cba2d0422e4f813c14ea252c5129fc621
chlcummi-debug <chlcummi@ucsc.edu>
2026-04-24 18:18:27 -0700

    iwerij

--- exercises/ex04.js
+++ exercises/ex04.js
@@ -1,8 +1,21 @@
-let count = 0;
+let count=0;
+let colors=["Orchid", "Coral", "HotPink", "Plum", "LightPink", "PaleVioletRed", "MediumVioletRed"  ];
+let colorCount;
 
-$("#needy-button").click(function () {
+$("#needy-button").click( function(){
 
-    count = count + 1;
-    $("#needy-button").html("I was clicked " + count)
+    count = count +1; 
+
+    colorCount=count-1;
+
+    $("#needy-button").html("you clicked me " + count + " and your favourite color today is: " + colors[colorCount] );
+
+    $("#needy-button").css( "background-color", colors[colorCount]);  
+
+    $("html").css( "background-color", colors[colorCount+1]);
+
+// things to try out:
+// make the page background color two colors ahead of the button color
+// make the page background color one color beyond of the button color
 
 });
