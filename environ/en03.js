e21266be8e3a5a20f5e1eda3744a2dc39582299e
chlcummi-debug <chlcummi@ucsc.edu>
2026-06-04 12:24:12 -0700

    hfhfh

--- environ/en03.js
+++ environ/en03.js
@@ -3,9 +3,26 @@ let environmentTitle = "Chelsea the Creator";
 let environmentElements = ["curtain", "dust", "warm floor", "half-open door"];
 
 let mainEntity = {
-    name: "Flower Girl ",
+    name: "Flower Girl",
     type: "soft border",
-    mood: " unique",
+    mood: "unique",
     isMoving: true,
     favoriteElement: environmentElements[1]
 };
+
+function changeMood(mood) {
+
+    if (mood == "happy") {
+        $("body").css("background-color", "pink");
+        $("#environment-output").html("Flower Girl feels happy.");
+    } else {
+        $("body").css("background-color", "lightblue");
+        $("#environment-output").html("Flower Girl feels calm.");
+    }
+
+}
+
+$("#env-button").click(function () {
+    let userInput = prompt("Type happy or calm");
+    changeMood(userInput);
+});
